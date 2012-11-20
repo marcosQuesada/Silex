a910bdf0220f28f4be8307d770f7aace8fc9e823
marcosQuesada <marcos.quesadas@gmail.com>
2012-11-21 00:39:50 +0100

    added debugging on index_dev

--- web/index_dev.php
+++ web/index_dev.php
@@ -1,12 +1,16 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\ClassLoader\DebugClassLoader;
+use Symfony\Component\HttpKernel\Debug\ErrorHandler;
+use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
+
 ini_set('display_errors', 1);
 error_reporting(-1);
-//DebugClassLoader::enable();
+DebugClassLoader::enable();
 
 if ('cli' !== php_sapi_name()) {
-    //ExceptionHandler::register();
+    ExceptionHandler::register();
 }
 
 $app = require_once __DIR__.'/../src/Core/app.php';
